1b82c58db2d168788a4f290e466f03d4ac6f4f06
Myat-Thiha <mthiha@go.pasadena.edu>
2025-05-27 19:28:18 -0700

    finished Lab8

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,14 +45,30 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if ('serviceWorker' in navigator) {
+    // If it is supported, we can register our service worker
+    console.log('Service Worker is supported');
+  } else {
+    console.warn('Service Worker is not supported in this browser');
+    return; // Exit if service workers are not supported
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
+  window.addEventListener('load', () => {
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
+    navigator.serviceWorker.register('./sw.js')
+      .then((registration) => {
+        // B4. TODO - Once the service worker has been successfully registered,
+        //            console log that it was successful.
+        console.log('Service Worker registered successfully with scope:', registration.scope);
+      })
+      .catch((error) => {
+        // B5. TODO - In the event that the service worker registration fails,
+        //            console log that it has failed.
+        console.error('Service Worker registration failed:', error);
+      });
+  });
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
@@ -68,21 +84,45 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  const storedRecipes = localStorage.getItem('recipes');
+  if (storedRecipes) {
+    // If recipes are found, parse them from string to an array of objects
+    return JSON.parse(storedRecipes);
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  const recipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
   /**************************/
+  return new Promise(async(resolve, reject) => {
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
   // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
   //            declared above
+  for (let i = 0; i < RECIPE_URLS.length; i++) {
+    const url = RECIPE_URLS[i];
+    try {
+      const response = await fetch(url);
+      const recipe = await response.json();
+      recipes.push(recipe);
+      if (recipes.length === RECIPE_URLS.length) {
+        saveRecipesToStorage(recipes);
+        resolve(recipes);
+      }
+    } catch (error) {
+      console.error('Failed to fetch recipe from:', url, error);
+      reject(error);
+      return;
+    }
+  }
+});
   // A5. TODO - Since we are going to be dealing with asynchronous code, create
   //            a try / catch block. A6-A9 will be in the try portion, A10-A11
   //            will be in the catch portion.
@@ -100,6 +140,7 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+
 }
 
 /**
